06a403d56641e1942ecae52e8bbbb1c005025230
Donnovin Wiebe <donnovinwiebe@protonmail.com>
2026-01-28 21:50:14 -0500

    Disabled mouse capture as it is unnecessary.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ mod ui;
 use std::io::Result;
 use std::{fs, io};
 use std::path::PathBuf;
-use ratatui::crossterm::event::{DisableMouseCapture, EnableMouseCapture};
 use ratatui::crossterm::execute;
 use ratatui::crossterm::terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
 use ratatui::prelude::CrosstermBackend;
@@ -16,7 +15,7 @@ fn main() -> Result<()> {
     // terminal setup
     enable_raw_mode()?;
     let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    execute!(stdout, EnterAlternateScreen)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
@@ -50,7 +49,6 @@ fn main() -> Result<()> {
     execute!(
         terminal.backend_mut(),
         LeaveAlternateScreen,
-        DisableMouseCapture
     )?;
     terminal.show_cursor()?;
 
